1af4c3a98d8d241561f0ceaee711f29ab292cca2
Christian Dangl <boxblinkracer@gmx.net>
2020-06-30 01:26:14 +0200

    add variables unit tests

---
+++ tests/Models/Components/Variables/VariablesTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Models\Components\Variables;
+
+
+use Orca\Components\Variables\Variables;
+use PHPUnit\Framework\TestCase;
+
+class VariablesTest extends TestCase
+{
+
+    /**
+     * This test verifies that our image
+     * variable is always present
+     */
+    public function testOrcaImage()
+    {
+        $varBuilder = new Variables('dev', 'latest');
+
+        $variables = $varBuilder->prepareVariables(array());
+
+        $this->assertEquals('dev', $variables['orca']['image']);
+    }
+
+    /**
+     * This test verifies that our tag
+     * variable is always present
+     */
+    public function testOrcaTag()
+    {
+        $varBuilder = new Variables('dev', 'latest');
+
+        $variables = $varBuilder->prepareVariables(array());
+
+        $this->assertEquals('latest', $variables['orca']['tag']);
+    }
+
+    /**
+     * This test verifies that our generation date
+     * variable is always present
+     */
+    public function testOrcaGenerateDate()
+    {
+        $varBuilder = new Variables('dev', 'latest');
+
+        $variables = $varBuilder->prepareVariables(array());
+
+        $this->assertEquals(date("D M d, Y G:i"), $variables['orca']['generate_date']);
+    }
+    
+}
